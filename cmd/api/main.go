cafe665e41957540ba964d271c31eee5270b5b1b
tnp2004 <iceandiceisice2004@gmail.com>
2024-06-26 18:47:03 +0700

    :recycle: Refactor server startup logic for graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,15 +2,9 @@ package main
 
 import (
 	"catalk/internal/server"
-	"fmt"
 )
 
 func main() {
-
 	server := server.NewServer()
-
-	err := server.ListenAndServe()
-	if err != nil {
-		panic(fmt.Sprintf("cannot start server: %s", err))
-	}
+	server.Start()
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,10 +1,14 @@
 package server
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 	"time"
 
 	_ "github.com/joho/godotenv/autoload"
@@ -14,11 +18,15 @@ import (
 
 type Server struct {
 	port int
-
-	db database.Service
+	db   database.Service
 }
 
-func NewServer() *http.Server {
+type ServerWrapper struct {
+	hostName   string
+	httpServer *http.Server
+}
+
+func NewServer() *ServerWrapper {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
 	NewServer := &Server{
 		port: port,
@@ -27,13 +35,45 @@ func NewServer() *http.Server {
 	}
 
 	// Declare Server config
-	server := &http.Server{
-		Addr:         fmt.Sprintf(":%d", NewServer.port),
-		Handler:      NewServer.RegisterRoutes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+	server := &ServerWrapper{
+		hostName: os.Getenv("HOST_NAME"),
+		httpServer: &http.Server{
+			Addr:         fmt.Sprintf(":%d", NewServer.port),
+			Handler:      NewServer.RegisterRoutes(),
+			IdleTimeout:  time.Minute,
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 30 * time.Second,
+		},
 	}
 
 	return server
+}
+
+func (s *ServerWrapper) Start() {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	go s.gracefulShutdown(ch)
+
+	s.httpListening()
+}
+
+func (s *ServerWrapper) httpListening() {
+	log.Printf("Server listening on %s%s%s", "http://", s.hostName, s.httpServer.Addr)
+	err := s.httpServer.ListenAndServe()
+
+	if err != nil && err != http.ErrServerClosed {
+		log.Printf("error listening server. Err: %s", err.Error())
+	}
+}
+
+func (s *ServerWrapper) gracefulShutdown(ch chan os.Signal) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	<-ch
+	log.Println("Server gracefully shutdown")
+
+	if err := s.httpServer.Shutdown(ctx); err != nil {
+		log.Fatalf("error while shutting down server. Err: %s", err.Error())
+	}
 }
