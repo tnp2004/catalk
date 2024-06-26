232a67d9085236e40bc24112238b28e28de6b9b1
tnp2004 <iceandiceisice2004@gmail.com>
2024-06-26 21:55:13 +0700

    :zap: Add TextGemini and newAiClient function

---
+++ internal/ai/gemini.go
@@ -0,0 +1,40 @@
+package ai
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+var clientInstance *genai.Client
+
+func TextGemini(msg string) {
+	ctx := context.Background()
+
+	client := newAiClient(ctx)
+	model := client.GenerativeModel("gemini-1.5-flash")
+
+	resp, err := model.GenerateContent(ctx, genai.Text(msg))
+	if err != nil {
+		log.Printf("error generate content. Err: %s", err.Error())
+	}
+
+	fmt.Printf("%#v", resp)
+}
+
+func newAiClient(ctx context.Context) *genai.Client {
+	if clientInstance != nil {
+		return clientInstance
+	}
+
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GOOGLE_API_KEY")))
+	if err != nil {
+		log.Printf("error text gemini. Err: %s", err.Error())
+	}
+
+	return client
+}
