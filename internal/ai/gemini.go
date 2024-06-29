a97141eb5bcc64424d7256e6627d6e48cbee3d61
tnp2004 <iceandiceisice2004@gmail.com>
2024-06-29 17:08:42 +0700

    :recycle: Refactor function to let ai know chat history

--- internal/ai/gemini.go
+++ internal/ai/gemini.go
@@ -12,33 +12,55 @@ import (
 
 var clientInstance *genai.Client
 
-func TextToGemini(body *GeminiTextBody) (*GeminiResponse, error) {
-	contentResp, err := sendMsgToGemini(body.Message)
+func TextToGemini(req *GeminiRequest) (*GeminiResponse, error) {
+	contentResp, err := sendMsgToGemini(req)
+
 	if err != nil {
 		return nil, err
 	}
 
-	resp := &GeminiResponse{Message: contentResp.Candidates[0].Content.Parts[0]}
+	newMessageHistory := append(req.MessageHistory, contentResp...)
+	resp := &GeminiResponse{
+		NewMessageHistory: newMessageHistory,
+		ResponseMessage:   contentResp[len(contentResp)-1].Message,
+	}
 
 	return resp, nil
 }
 
-func sendMsgToGemini(msg string) (*genai.GenerateContentResponse, error) {
+func sendMsgToGemini(req *GeminiRequest) ([]*MessageInfo, error) {
 	ctx := context.Background()
 
 	client, err := newAiClient(ctx)
 	if err != nil {
 		return nil, err
 	}
-	model := client.GenerativeModel("gemini-1.5-flash")
 
-	resp, err := model.GenerateContent(ctx, genai.Text(msg))
+	model := client.GenerativeModel("gemini-1.5-flash")
+	cs := model.StartChat()
+	msgHistoryContents := convertMessageHistoryToParts(req.MessageHistory)
+	cs.History = append(cs.History, msgHistoryContents...)
+
+	resp, err := cs.SendMessage(ctx, genai.Text(req.NewUserMessage))
 	if err != nil {
 		log.Printf("error generate content. Err: %s", err.Error())
 		return nil, fmt.Errorf("generate content error: %s", err.Error())
 	}
+	r := make([]*MessageInfo, 0)
+	r = append(r, &MessageInfo{
+		Message: req.NewUserMessage,
+		Role:    "user",
+	})
+	for _, part := range resp.Candidates[0].Content.Parts {
+		if txt, ok := part.(genai.Text); ok {
+			r = append(r, &MessageInfo{
+				Message: string(txt),
+				Role:    resp.Candidates[0].Content.Role,
+			})
+		}
+	}
 
-	return resp, nil
+	return r, nil
 }
 
 func newAiClient(ctx context.Context) (*genai.Client, error) {
--- internal/ai/models.go
+++ internal/ai/models.go
@@ -1,9 +1,37 @@
 package ai
 
-type GeminiTextBody struct {
+import (
+	"github.com/google/generative-ai-go/genai"
+)
+
+type GeminiRequest struct {
+	MessageHistory []*MessageInfo `json:"messageHistory"`
+	NewUserMessage string         `json:"newUserMessage"`
+}
+
+type MessageInfo struct {
 	Message string `json:"message"`
+	Role    string `json:"role"`
 }
 
 type GeminiResponse struct {
-	Message any `json:"message"`
+	NewMessageHistory []*MessageInfo `json:"newMessageHistory"`
+	ResponseMessage   any            `json:"responseMessage"`
+}
+
+func convertMessageHistoryToParts(msgInfo []*MessageInfo) []*genai.Content {
+	contents := make([]*genai.Content, 0)
+
+	for _, v := range msgInfo {
+		c := &genai.Content{
+			Parts: []genai.Part{
+				genai.Text(v.Message),
+			},
+			Role: v.Role,
+		}
+
+		contents = append(contents, c)
+	}
+
+	return contents
 }
