402689a438ca05f63e8342f8b410e82ddbe7f0c0
tnp2004 <iceandiceisice2004@gmail.com>
2024-07-04 14:45:59 +0700

    :zap: Add description of cat breeds

--- instructions/breeds.go
+++ instructions/breeds.go
@@ -1,5 +1,18 @@
 package instructions
 
 var CatBreedsMap = map[string]string{
-	"persian": "persian",
+	"maineCoon":        "maineCoon",
+	"siamese":          "siamese",
+	"persian":          "persian",
+	"bengal":           "bengal",
+	"sphynx":           "sphynx",
+	"britishShorthair": "britishShorthair",
+	"ragdoll":          "ragdoll",
+	"scottishFold":     "scottishFold",
+	"russianBlue":      "russianBlue",
+	"abyssinian":       "abyssinian",
+	"norwegianForest":  "norwegianForest",
+	"devonRex":         "devonRex",
+	"burmese":          "burmese",
+	"bombay":           "bombay",
 }
