492a4abfcd916595ec174668865b0ed7642e40a1
tnp2004 <iceandiceisice2004@gmail.com>
2024-07-12 22:23:49 +0700

    refactor: create react component folder

--- cmd/web/src/components/react/ChatBox.tsx
+++ cmd/web/src/components/react/ChatBox.tsx
@@ -1,5 +1,5 @@
 import { useState, type FormEvent } from "react"
-import { MessageElement } from "./MessageElement";
+import { MessageElement } from "../react/MessageElement";
 
 export const ChatBox = ({ breed }: { breed: string }) => {
     const [chat, setChat] = useState<MessageHistory[]>([])
@@ -7,7 +7,7 @@ export const ChatBox = ({ breed }: { breed: string }) => {
 
     const SendMessageToAI = async (e: FormEvent) => {
         e.preventDefault();
-    
+
         const headers: Headers = new Headers();
         headers.set("Content-Type", "application/json");
         headers.set("Accept", "application/json");
@@ -35,19 +35,21 @@ export const ChatBox = ({ breed }: { breed: string }) => {
     return (
         <>
             <ul className="h-[60vh] flex flex-col px-2 py-3 gap-2 overflow-auto">
-                {chat.map(({message, role}, i) => <MessageElement key={`${role}message-${i+1}`} message={message} role={role} />)}
+                {chat.map(({ message, role }, i) => <MessageElement key={`${role}message-${i + 1}`} message={message} role={role} />)}
             </ul>
-            
+
             <form onSubmit={SendMessageToAI} className="flex mb-2">
                 <input
                     type="text"
                     value={msgInput}
                     onChange={e => setMsgInput(e.target.value)}
-                    className="w-full px-2 rounded-md shadow-sm"
+                    className="w-full px-2 rounded-l-md shadow-sm"
                     placeholder="Type a message . . ."
                     required
                 />
-                <button className="font-bold p-1" type="submit">send</button>
+                <button className="rounded-r font-bold py-1 px-2 bg-slate-600 hover:bg-slate-700 text-slate-200 hover:text-slate-100" type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m22 2-7 20-4-9-9-4Z" /><path d="M22 2 11 13" /></svg>
+                </button>
             </form>
         </>
     )
