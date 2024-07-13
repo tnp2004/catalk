0d88aa498418d5bf83002c860aca84d6c4ea4108
tnp2004 <iceandiceisice2004@gmail.com>
2024-07-13 20:43:50 +0700

    perf: add htmlFor attribute for label

--- cmd/web/src/components/react/MessageElement.tsx
+++ cmd/web/src/components/react/MessageElement.tsx
@@ -1,7 +1,7 @@
 export const MessageElement = ({ message, role }: MessageHistory) => {
     return (
         <li className={`flex ${role == "user" ? "justify-end" : "justify-start"}`}>
-            <label
+            <label htmlFor="message"
                 className={`border-2 border-slate-700 bg-slate-100 shadow-md max-w-[32rem] rounded-xl ${role == "user" ? "rounded-br-sm" : "rounded-bl-sm"} block py-2 px-3`}
             >{message}</label>
         </li>
