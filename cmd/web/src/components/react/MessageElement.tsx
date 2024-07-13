0cacd8249fd9dd7df01211c2f717915ab13077c2
tnp2004 <iceandiceisice2004@gmail.com>
2024-07-13 20:00:08 +0700

    fix(chat): fix message box border display incorrectly

--- cmd/web/src/components/react/MessageElement.tsx
+++ cmd/web/src/components/react/MessageElement.tsx
@@ -1,8 +1,8 @@
 export const MessageElement = ({ message, role }: MessageHistory) => {
     return (
-        <li className={`flex justify-${role === "user" ? "end" : "start"}`}>
+        <li className={`flex ${role == "user" ? "justify-end" : "justify-start"}`}>
             <label
-                className={`border-2 border-slate-700 bg-slate-100 shadow-md rounded-xl rounded-${role == "user" ? "br" : "bl"}-sm block py-2 px-3`}
+                className={`border-2 border-slate-700 bg-slate-100 shadow-md rounded-xl ${role == "user" ? "rounded-br-sm" : "rounded-bl-sm"} block py-2 px-3`}
             >{message}</label>
         </li>
     )
