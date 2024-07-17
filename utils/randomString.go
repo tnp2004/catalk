6b0901e5bf0a51ba3ef35181c5a629dbf49882c4
tnp2004 <iceandiceisice2004@gmail.com>
2024-07-17 19:52:44 +0700

    chore: generate string utility

---
+++ utils/randomString.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"time"
+
+	"golang.org/x/exp/rand"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(rand.NewSource(uint64(time.Now().UnixNano())))
+
+func GenerateRandomString(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
